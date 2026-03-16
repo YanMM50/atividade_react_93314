cd5128ad424781a51906ab0d179e43ab81b5308c
Yan <yanmendes50@gmail.com>
2026-03-16 23:48:16 +0000

    criando bd

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ const sequelize = new Sequelize('db_projeto', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 })    
+
+const Cliente = sequelize.define('Cliente',{
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    telefone:{
+        type: DataTypes.STRING
+    }
+
+})
